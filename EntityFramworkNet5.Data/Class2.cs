699716b7c227e1628a8a658e972e03693d893a5d
igal <igal.nikolaev@gmail.com>
2022-11-18 10:17:39 +0200

    class 2 staged

---
+++ EntityFramworkNet5.Data/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramworkNet5.Data
+{
+    internal class Class2
+    {
+        public int ID2 { get; set; }
+    }
+}
